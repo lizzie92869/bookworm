f3e7e7fb08c9fef408debf469d4415a4607ecfc2
Jordan Sleeper <survivingknowledge@gmail.com>
2017-08-20 16:06:56 -0400

    Books migration

--- db/migrate/20170820195735_create_books.rb
+++ db/migrate/20170820195735_create_books.rb
@@ -1,8 +1,13 @@
 class CreateBooks < ActiveRecord::Migration[5.1]
   def change
     create_table :books do |t|
-
+      t.string :title
+      t.string :book_code
+      t.string :description
       t.timestamps
     end
+
+    #force book_code to be unique
+    add_index :books, :book_code, unique: true
   end
 end
