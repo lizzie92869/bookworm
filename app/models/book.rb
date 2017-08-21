562a179eb2ea600d0a0393859f346e15dc268b81
Jordan Sleeper <survivingknowledge@gmail.com>
2017-08-21 00:30:50 -0400

    Added book default image and scope class methods

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,7 +1,7 @@
 class Book < ApplicationRecord
   #paperclip
   has_attached_file :cover_image, styles: { medium: "300x300", thumb: "75x150" },
-                                  default_url: ""
+                                  default_url: "black-noimg.jpg"
 
   validates_attachment_content_type :cover_image, content_type: /\Aimage\/.*\z/
 
@@ -14,5 +14,16 @@ class Book < ApplicationRecord
   validates :description, length: { maximum: 500, too_long: "%{count} characters is the maximum allowed for description" },
                           allow_blank: true
 
+  #scopes (chainable class methods)
+  #have the if <>.present? on any input
+  #you will have to override this order with reorder(colum: :direction)
+  default_scope { order(created_at: :desc) }
 
+  scope :by_book_code, ->(book_code) { where(book_code: book_code) if book_code.present? }
+  #search by title
+  scope :by_title, ->(title) { where('title LIKE ?', "%#{title}%") if title.present? }
+
+  #should these be moved?
+  scope :order_by_title_asc, -> { reorder(title: :asc) }
+  scope :order_by_title_desc, -> { reorder(title: :desc) }
 end
