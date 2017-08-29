7f83efb99c2b86f2fc3208c90bf634894959465c
lizzie <lizzie.sorin@hotmail.fr>
2017-08-28 21:40:41 -0700

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,35 @@ post '/user' => 'users#create'
 post '/session' => 'sessions#create'
 get 'sessions/new'
 
+books
+
+ get '/books', to: 'books#index'
+ get '/books/new', to: 'books#new'
+ get '/books/:id', to: 'books#show'
+ get '/books/:id/edit', to: 'books#edit'
+ patch '/books/:id', to: 'books#update'
+ put '/books/:id', to: 'books#update'
+ delete '/books/:id', to: 'books#destroy'
+ post '/books', to: 'books#create'
+
+ users
+
+
+ /users
+ get '/users/new', to: 'users#new' => show signup  form
+ post '/users', to: 'users#create'
+
+ get '/signup', to: 'users#new'
+ post '/signup', to: 'users#create'
+
+ session, we control login process
+http://site.com/<whatto type here>
+get '/signin', to: 'sessions#new'
+post '/signin', to: 'sessions#create'
+delete '/logout', to: 'sessions#destroy'
+
+
+
+
+
 end
