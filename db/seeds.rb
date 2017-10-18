459e748e9f4755e08fe1f59f7472284c7754b593
Lizzie <lizzie.sorin@hotmail.fr>
2017-10-17 17:32:31 -0700

    database to populate correctly

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,4 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 User.create([{ name: 'lizzie' }, { name: 'luca' }, { name: 'Peter' }])
+Book.create(title: "Crazy little thing", book_code: "1", description: "feets on the beach", cover_image_file_name: "book1.png", cover_image_content_type: "image", cover_image_file_size: 97, chapters_count: 1)
